83965461ac3f4b1da231531c97ace368b169c4e3
Gary Zlobinskiy <garzlo22@bergen.org>
2019-11-09 14:07:11 -0500

    camera added with capture button

--- recipEats/App.js
+++ recipEats/App.js
@@ -1,38 +1,29 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React  from 'react';
+import { StyleSheet, View } from 'react-native';
+import Camera from './components/Camera.js';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
+export default class App extends React.Component {
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+    constructor(props){
+        super(props);
+        process.nextTick = setImmediate;
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>test1</Text>
+                <Camera />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 });
