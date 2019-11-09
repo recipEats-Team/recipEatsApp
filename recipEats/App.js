d9f7281db6dfc1a273e49f7eb2994f8771c76151
Gary Zlobinskiy <garzlo22@bergen.org>
2019-11-09 15:57:11 -0500

    fixed camera expo camera

--- recipEats/App.js
+++ recipEats/App.js
@@ -1,28 +1,84 @@
-import React  from 'react';
-import { StyleSheet, View } from 'react-native';
-import Camera from './components/Camera.js';
+// import React  from 'react';
+// import { StyleSheet, View } from 'react-native';
+// import Camera from './components/Camera.js';
+//
+// export default class App extends React.Component {
+//
+//     constructor(props){
+//         super(props);
+//         process.nextTick = setImmediate;
+//     }
+//
+//     render() {
+//         return (
+//             <View style={styles.container}>
+//                 <Camera />
+//             </View>
+//         );
+//     }
+// }
+//
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//     }
+// });
+//
+import React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { Camera } from 'expo-camera';
 
-export default class App extends React.Component {
+export default class CameraExample extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+  };
 
-    constructor(props){
-        super(props);
-        process.nextTick = setImmediate;
-    }
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Camera />
+  render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.setState({
+                    type:
+                      this.state.type === Camera.Constants.Type.back
+                        ? Camera.Constants.Type.front
+                        : Camera.Constants.Type.back,
+                  });
+                }}>
+                <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+              </TouchableOpacity>
             </View>
-        );
+          </Camera>
+        </View>
+      );
     }
+  }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-});
