9842e02b1c22b6d731574dba6a0a969609bedbba
Gary Zlobinskiy <garzlo22@bergen.org>
2019-11-09 16:06:31 -0500

    removed git ignore file

--- recipEats/App.js
+++ recipEats/App.js
@@ -1,34 +1,5 @@
-// import React  from 'react';
-// import { StyleSheet, View } from 'react-native';
-// import Camera from './components/Camera.js';
-//
-// export default class App extends React.Component {
-//
-//     constructor(props){
-//         super(props);
-//         process.nextTick = setImmediate;
-//     }
-//
-//     render() {
-//         return (
-//             <View style={styles.container}>
-//                 <Camera />
-//             </View>
-//         );
-//     }
-// }
-//
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         backgroundColor: '#fff',
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//     }
-// });
-//
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet, TouchableHighlight, Button } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
 
@@ -75,6 +46,9 @@ export default class CameraExample extends React.Component {
                 }}>
                 <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
               </TouchableOpacity>
+              <TouchableHighlight style={styles.captureButton} disabled={this.props.buttonDisabled}>
+                <Button onPress={this.props.onClick} disabled={this.props.buttonDisabled} title="Capture" accessibilityLabel="Learn more about this button"/>
+              </TouchableHighlight>
             </View>
           </Camera>
         </View>
@@ -82,3 +56,12 @@ export default class CameraExample extends React.Component {
     }
   }
 }
+
+const styles = StyleSheet.create({
+  captureButton: {
+    width: 160,
+    height: 50,
+    borderRadius: 10,
+    backgroundColor: "white",
+  }
+});
