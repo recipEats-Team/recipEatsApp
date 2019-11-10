d12b4f39be0fd67fa4a3e4550b126b9120bc2e83
valkim21 <valerieelliekim@gmail.com>
2019-11-09 20:32:22 -0500

    adding api call

---
+++ recipEats/recipe_api/apiTest.js
@@ -0,0 +1,27 @@
+//https://www.taniarascia.com/how-to-connect-to-an-api-with-javascript/
+
+//Retrieving the data with an HTTP request
+//var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var XMLHttpRequest = require("xhr2");
+var request = new XMLHttpRequest;
+
+request.open('GET', 'https://www.food2fork.com/api/search?key=b608fc52e7a39e465582bd652ae336d9&q=chicken%20breast&page=2', true)
+request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+        var data = JSON.parse(this.response);
+        console.log(data);
+    } else {
+        console.log("error");
+    }
+};
+
+
+
+request.send()
+
+
+/*
+data.forEach(recipe => {
+    console.log(recipe)
+})
+*/
