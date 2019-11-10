58379dad258100e20127fca063466413df17b39e
valkim21 <valerieelliekim@gmail.com>
2019-11-09 22:40:00 -0500

    api mid fix

--- recipEats/recipe_api/apiTest.js
+++ recipEats/recipe_api/apiTest.js
@@ -5,27 +5,25 @@
 var XMLHttpRequest = require("xhr2");
 var request = new XMLHttpRequest;
 
+
+var recipes = [];
+
 request.open('GET', 'https://www.food2fork.com/api/search?key=b608fc52e7a39e465582bd652ae336d9&q=bacon,tomatot,lettuce,bread&page=2', true)
 request.onload = function() {
     if (request.status >= 200 && request.status < 400) {
         var data = JSON.parse(this.response);
         var topThree = data.recipes.slice(0,3);
         for(var element of topThree){
-            console.log(element.title);
-            console.log("\tRecipe URL: " + element.source_url)
+            var currentRecipe = {};
+            currentRecipe["recipe_title"] = element.title;
+            currentRecipe["recipe_url"] = element.source_url;
+            currentRecipe["recipe_image"] = element.image_url;
+            recipes.push(currentRecipe);
         }
     } else {
         console.log("error");
     }
+    console.log(recipes);
 };
 
-
-
 request.send()
-
-
-/*
-data.forEach(recipe => {
-    console.log(recipe)
-})
-*/
